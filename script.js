db2b0f1a9f83c4f032c4a6b4c3143ec72bf45443
Keith Underwood <p4nic510@gmail.com>
2016-04-30 14:37:09 -0600

    rework project

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-var rows = 16;
-var colums = 16;
-var $row = $("<div />", {class: 'row'});
-var $square = $("<div />", {class: 'square'});
 
 $(document).ready(function() {
-  $('body').append('<div id="wrapper"></div>');
-    for (var i = 0; i < colums; i++) {
-      $row.append($square.clone());
-    };
+  var gridSize = 16
+  var squareSize = 768 / gridSize
 
-    for(var i = 0; i <rows; i++) {
-      $("#wrapper").append($row.clone());
-    };
+  $('body').append('<div id ="wrapper"></div>');
+  $('#wrapper').css({'text-align': 'center'})
+  $('#wrapper').append('<button type="button">New Grid</button>');
+  $('button').css({
+    width: '71px',
+    'margin': '0px auto 10px auto',
+  });
+  $('#wrapper').append('<div id ="grid"></div>');
+  $('#grid').height('768px').width('768px');
+  $('#grid').css({
+    margin: '0px auto',
+    border: '1px solid'});
+
+    for (var i = 0; i < gridSize * gridSize; i++) {
+          $('#grid').append('<div class = "square"></div>')
+        };
+  $('.square').height(squareSize).width(squareSize);
+  $('.square').css({
+    'vertical-align': 'top',
+    display: 'inline-block',
+    'background-color': 'white',
+  });
+
 
     $(".square").hover(function() {
        $(this).css("background-color","orange");
     });
 
   console.log("Grid complete")
+
+  $('button').click(function() {
+    $('.square').remove();
+
+    var gridSize = prompt("choode a number between 1 and 32!");
+    var squareSize = 768 / gridSize
+
+    if (gridSize >= 1 && gridSize <= 32) {
+      for (var i = 0; i < gridSize * gridSize; i++) {
+            $('#grid').append('<div class = "square"></div>')
+          };
+    } else {
+      return alert('That was not 1-32... Click "New Grid" and try again.')
+    };
+
+    $('.square').height(squareSize).width(squareSize);
+    $('.square').css({
+      'vertical-align': 'top',
+      display: 'inline-block',
+      'background-color': 'white',
+    });
+
+    $(".square").hover(function() {
+       $(this).css("background-color","orange");
+    });
+
+
+  });
 });
